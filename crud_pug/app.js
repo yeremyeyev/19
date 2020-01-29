bfa7f5079875c96a2833ee0193d10421d430e675
Eremeev Andery <a.yeremyeyev@gmail.com>
2020-01-29 22:09:32 +0200

    29_01_2020

--- crud_pug/app.js
+++ crud_pug/app.js
@@ -4,57 +4,59 @@ let courses = require('./data/courses.json');
 const app = express();
 
 app.set('view engine', 'pug');
-// 
 
 
 
-app.use(express.urlencoded());
-// app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.use(express.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 
-app.get('/', function(req, res){
-  res.render('index', {title: 'CRUD demo'});
+app.get('/', function(req, res) {
+  res.render('index', {title: 'CRUD demo' });
 });
 
-app.get('/courses', function(req, res){
+app.get('/courses', function(req, res) {
   res.render('courses', {
-    title:"CRUD Курсы",
+    title: 'CRUD Курсы',
     courses: courses
   });
 });
-app.get('/courses/add', function(req, res){
+app.get('/courses/add', function(req, res) {
   res.render('add');
 });
 
-app.post('/courses/add', function(req, res){
-  let courses = {
+
+app.post('/courses/add', function(req, res) {
+  let course = {
     name: req.body.name,
-    id:Date.now()
+    id: Date.now()
   };
-  courses.push(courses);
+  courses.push(course);
   res.redirect('/courses');
 });
 
-app.get('/coursesedit/:id', function(req, res){
-  let course = courses.find(function(course){
+
+
+app.get('/courses/edit/:id', function(req, res) {
+  let course = courses.find(function(course) {
     return course.id === parseInt(req.params.id);
   });
 
+  if (!course) {
+    res.sendStatus(404);
+    return;
+  }
 
-if (!course) {
-  res.sendStatus(404);
-  return;  
-}
-
-res.render('edit', {course: course});
+  res.render("edit", {course: course});
 });
 
-app.post('/courses/edit/:id', function(req, res){
-  let course = courses.find(function(course){
+app.post('/courses/edit/:id', function(req, res) {
+  let course = courses.find(function(course) {
     return course.id === parseInt(req.params.id);
   });
-  if (!course){
+  if (!course) {
     res.sendStatus(404);
     return;
   }
@@ -63,13 +65,15 @@ app.post('/courses/edit/:id', function(req, res){
   res.redirect('/courses');
 });
 
-app.get('/courses/delete/:id', function(req, res){
-  courses = courses.filter(function(courses){
-    return course.id !==parseInt(req.params.id);
+
+app.get('/courses/delete/:id', function(req, res) {
+  
+  courses = courses.filter(function(course) {
+    return course.id !== parseInt(req.params.id);
   });
   res.redirect('/courses');
 });
 
-app.listen(3000, function(){
-  console.log('app at http://')
+app.listen(3000, function() {
+  console.log("app at http://localhost:3000");
 });
